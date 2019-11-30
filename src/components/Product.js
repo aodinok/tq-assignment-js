ec8039a1d45631b0baaefb7f3b077cdd97b573c3
Oleksandr Odinok <oleksandr.odinok@quantcast.com>
2019-11-30 22:30:37 +0200

    Make product card it responsive

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,12 +7,14 @@ class Product extends React.PureComponent {
   render() {
     const { productUrl, image, title, summary, price } = this.props;
     return (
-      <a href={productUrl} className="tq-product">
-        <img className="tq-product-img" src={image} alt={title} />
+      <a href={productUrl} className="tq-product" rel="noopener noreferrer" target="_blank">
+        <div className="tq-product-img-container">
+          <img className="tq-product-img" src={image} alt={title} />
+        </div>
         <div className="tq-product-content">
           <h3 className="tq-product-title">{title}</h3>
           <p className="tq-product-summary">{summary}</p>
-          <p className="tq-product-price">{price}</p>
+          <p className="tq-product-price">€{parseFloat(price).toFixed(2) || 0}</p>
         </div>
       </a>
     );
